693af9713033baf9bf117080cf3acd063b25b3f1
xichuang <1690680531@qq.com>
2023-06-13 17:25:30 +0800

    feat: 文档搬运工

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,183 @@
+module.exports = {
+  title: '文档搬运工',
+  description: '学习笔记，包含前端开发、后端开发、服务器运维等',
+  // 注入到当前页面的 HTML <head> 中的标签
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+  ],
+  host: 'localhost',
+  port: '9000',
+  base: '/', // 这是部署到github相关的配置 下面会讲
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  themeConfig: {
+    sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
+    lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
+    // displayAllHeaders: true,
+    // permalink: "/:year/:month/:day/:slug",
+    nav: [
+      {
+        text: '前端',
+        items: [
+          { text: '前端导航', link: '/frontend/feds' },
+          { text: 'CSS3', link: '/frontend/css' },
+          { text: 'Electron', link: '/frontend/electron' },
+          { text: 'ES6', link: '/frontend/es6' },
+          { text: 'Styled Components', link: '/frontend/styled-components' },
+          { text: 'Stylus', link: '/frontend/stylus' },
+          { text: 'Sass', link: '/frontend/sass' },
+          { text: 'HTML', link: '/frontend/html' },
+          { text: 'JavaScript', link: '/frontend/javascript' },
+          { text: 'jQuery', link: '/frontend/jquery' },
+          { text: 'Less', link: '/frontend/lessjs' },
+          { text: 'Next', link: '/frontend/nextjs' },
+          { text: 'React', link: '/frontend/react' },
+          { text: 'React Router', link: '/frontend/reactrouter' },
+          { text: 'React Native', link: '/frontend/react-native' },
+          { text: 'Regex 正则表达式', link: '/frontend/regex' },
+          { text: 'TypeScript', link: '/frontend/typescript' },
+          { text: 'Tailwind CSS', link: '/frontend/tailwindcss' },
+          { text: 'Vue2', link: '/frontend/vue2' },
+          { text: 'Vue3', link: '/frontend/vue' },
+        ]
+      },
+      {
+        text: '编程',
+        items: [
+          { text: 'Bash', link: '/program/bash' },
+          { text: 'C', link: '/program/c' },
+          { text: 'C++', link: '/program/cpp' },
+          { text: 'Dart', link: '/program/dart' },
+          { text: 'Docker', link: '/program/docker' },
+          { text: 'Flutter', link: '/program/flutter' },
+          { text: 'Golang', link: '/program/golang' },
+          { text: 'GraphQL', link: '/program/graphql' },
+          { text: 'Java', link: '/program/java' },
+          { text: 'JSON', link: '/program/json' },
+          { text: 'Kubernetes', link: '/program/kubernetes' },
+          { text: 'Markdown', link: '/program/markdown' },
+          { text: 'MATLAB', link: '/program/matlab' },
+          { text: 'PHP', link: '/program/php' },
+          { text: 'Python', link: '/program/python' },
+          { text: 'Ruby', link: '/program/ruby' },
+          { text: 'Rust', link: '/program/rust' },
+          { text: 'Swift', link: '/program/swift' },
+          { text: 'SwiftUI', link: '/program/swiftui' },
+          { text: 'YAML', link: '/program/yaml' },
+        ]
+      },
+      {
+        text: 'Nodejs',
+        items: [
+          { text: 'Express.js', link: '/nodejs/expressjs' },
+          { text: 'Jest', link: '/nodejs/jest' },
+          { text: 'Koa.js', link: '/nodejs/koajs' },
+          { text: 'Lerna', link: '/nodejs/lerna' },
+          { text: 'npm', link: '/nodejs/npm' },
+          { text: 'NestJS', link: '/nodejs/nestjs' },
+          { text: 'PM2', link: '/nodejs/pm2' },
+          { text: 'Package.json', link: '/nodejs/package.json' },
+          { text: 'Yarn', link: '/nodejs/yarn' },
+        ]
+      },
+      {
+        text: '工具',
+        items: [
+          { text: 'Emacs', link: '/tool/emacs' },
+          { text: 'Github', link: '/tool/github' },
+          { text: 'Nginx', link: '/tool/nginx' },
+          { text: 'Semver', link: '/tool/semver' },
+          { text: 'Vim', link: '/tool/vim' },
+          { text: 'XPath', link: '/tool/xpath' }, 
+        ]
+      },
+      {
+        text: '命令',
+        items: [
+          { text: 'ADB, Android Debug Bridge', link: '/command/adb' },
+          { text: 'Conan', link: '/command/conan' },
+          { text: 'Conda', link: '/command/conda' },
+          { text: 'CocoaPods', link: '/command/cocoapods' },
+          { text: 'Cargo', link: '/command/cargo' },
+          { text: 'Curl', link: '/command/curl' }, 
+          { text: 'Chmod', link: '/command/chmod' }, 
+          { text: 'Cron', link: '/command/cron' }, 
+          { text: 'CMake', link: '/command/cmake' }, 
+          { text: 'Make', link: '/command/make' }, 
+          { text: 'Git', link: '/command/git' }, 
+          { text: 'Grep', link: '/command/grep' }, 
+          { text: 'Find', link: '/command/find' }, 
+          { text: 'FFmpeg', link: '/command/ffmpeg' }, 
+          { text: 'Htop', link: '/command/htop' }, 
+          { text: 'Homebrew', link: '/command/homebrew' }, 
+          { text: 'Netstat', link: '/command/netstat' }, 
+          { text: 'iptables', link: '/command/iptables' }, 
+          { text: 'jq', link: '/command/jq' }, 
+          { text: 'Justfile', link: '/command/justfile' }, 
+          { text: 'Lsof', link: '/command/lsof' }, 
+          { text: 'Mitmproxy', link: '/command/mitmproxy' }, 
+          { text: 'Netcat', link: '/command/netcat' }, 
+          { text: 'Sed', link: '/command/sed' }, 
+          { text: 'OpenSSL', link: '/command/openssl' }, 
+          { text: 'ps', link: '/command/ps' }, 
+          { text: 'Systemd', link: '/command/systemd' }, 
+          { text: 'SSH', link: '/command/ssh' }, 
+          { text: 'Screen', link: '/command/screen' }, 
+          { text: 'YUM', link: '/command/yum' }, 
+          { text: 'CMD', link: '/command/cmd' }, 
+          { text: 'Zip', link: '/command/zip' }, 
+          { text: 'APT', link: '/command/apt' }, 
+          { text: 'tar', link: '/command/tar' }, 
+          { text: 'Linux Command', link: '/command/linux-command' }, 
+        ]
+      },
+      {
+        text: '数据库',
+        items: [
+          { text: 'MySQL', link: '/database/mysql' },
+          { text: 'MongoDB', link: '/database/mongodb' },
+          { text: 'Oracle', link: '/database/oracle' },
+          { text: 'PostgreSQL', link: '/database/postgres' },
+          { text: 'Redis', link: '/database/redis' }
+        ]
+      },
+      {
+        text: '快捷键',
+        items: [
+          { text: 'Adobe Lightroom', link: '/quicklink/adobe-lightroom' },
+          { text: 'Adobe Photoshop', link: '/quicklink/adobe-photoshop' },
+          { text: 'Adobe XD', link: '/quicklink/adobe-xd' },
+          { text: 'Android Studio', link: '/quicklink/android-studio' },
+          { text: 'Firefox', link: '/quicklink/firefox' },
+          { text: 'Gmail', link: '/quicklink/gmail' },
+          { text: 'Gitlab', link: '/quicklink/gitlab' },
+          { text: 'Google Chrome', link: '/quicklink/google-chrome' },
+          { text: 'IntelliJ IDEA', link: '/quicklink/intelli-j-idea' },
+          { text: 'PyCharm', link: '/quicklink/pycharm' },
+          { text: 'Sketch', link: '/quicklink/sketch' },
+          { text: 'Sublime Text', link: '/quicklink/sublime-text' },
+          { text: 'VSCode', link: '/quicklink/vscode' },
+          { text: 'WebStorm', link: '/quicklink/webstorm' },
+          { text: 'Xcode', link: '/quicklink/xcode' },
+        ]
+      },
+      {
+        text: '其它',
+        items: [
+          { text: 'Aspect Ratio', link: '/other/aspect-ratio' },
+          { text: 'Github Actions', link: '/other/github-actions' },
+          { text: 'Gitlab CI/CD', link: '/other/gitlab-ci' },
+          { text: 'Colors Named', link: '/other/colors-named' },
+          { text: 'HTTP 状态码', link: '/other/http-status-code' },
+          { text: 'HTML 字符实体', link: '/other/html-char' },
+          { text: 'ISO 639-1 Language Code', link: '/other/iso-639-1' },
+          { text: 'Emoji', link: '/other/emoji' },
+          { text: 'MIME types', link: '/other/mime' },
+          { text: 'Resolutions', link: '/other/resolutions' },
+        ]
+      },
+    ],
+    sidebar: 'auto',
+  },
+}
---
+++ docs/README.md
@@ -0,0 +1,71 @@
+---
+# home: true
+navbar: true
+isNoPage: true
+sidebar: true
+# pageClass: home-list
+---
+
+以下是一些备忘清单，为开发人员分享快速参考备忘清单【速查表】 :)。目的是为了方便技术栈查阅，这是我从网上搬运过来的。如果您发现此处的信息有不正确的地方，您可以通过issues给我留言，我会尽快查阅更正，如果你有更好的备忘清单也可以贴上，一起为开源贡献
+
+## 前端
+
+|     [前端导航](./frontend/feds.md)      |       [CSS 3](./frontend/css.md)       |    [Electron](./frontend/electron.md)     |         [ES 6](./frontend/es6.md)         | [Styled Components](./frontend/styled-components.md) |
+| :-------------------------------------: | :------------------------------------: | :---------------------------------------: | :---------------------------------------: | :--------------------------------------------------: |
+|     [Stylus](./frontend/stylus.md)      |       [Sass](./frontend/sass.md)       |        [HTML](./frontend/html.md)         |  [JavaScript](./frontend/javascript.md)   |            [jQuery](./frontend/jquery.md)            |
+|     [Less.js](./frontend/lessjs.md)     |    [Next.js](./frontend/nextjs.md)     |       [React](./frontend/react.md)        | [React Router](./frontend/reactrouter.md) |      [React Native](./frontend/react-native.md)      |
+| [RegEx 正则表达式](./frontend/regex.md) | [TypeScript](./frontend/typescript.md) | [Tailwind CSS](./frontend/tailwindcss.md) |        [Vue 2](./frontend/vue2.md)        |              [Vue 3](./frontend/vue.md)              |
+
+## 编程
+
+|       [Bash](./program/bash.md)       |        [C](./program/c.md)        |     [C++](./program/cpp.md)     |    [Dart](./program/dart.md)    | [Docker](./program/docker.md) |
+| :-----------------------------------: | :-------------------------------: | :-----------------------------: | :-----------------------------: | :---------------------------: |
+|    [Flutter](./program/flutter.md)    |   [Golang](./program/golang.md)   | [GraphQL](./program/graphql.md) |    [Java](./program/java.md)    |   [JSON](./program/json.md)   |
+| [Kubernetes](./program/kubernetes.md) | [Markdown](./program/markdown.md) |  [MATLAB](./program/matlab.md)  |     [PHP](./program/php.md)     | [Python](./program/python.md) |
+|       [Ruby](./program/ruby.md)       |     [Rust](./program/rust.md)     |   [Swift](./program/swift.md)   | [SwiftUI](./program/swiftui.md) |   [YAML](./program/yaml.md)   |
+
+## Nodejs
+
+| [Express.js](./nodejs/expressjs.md) | [Jest](./nodejs/jest.md) |       [Koa.js](./nodejs/koajs.md)        | [Lerna](./nodejs/lerna.md) | [npm](./nodejs/npm.md) |
+| :---------------------------------: | :----------------------: | :--------------------------------------: | :------------------------: | :--------------------: |
+|    [NestJS](./nodejs/nestjs.md)     |  [PM2](./nodejs/pm2.md)  | [package.json](./nodejs/package.json.md) |  [Yarn](./nodejs/yarn.md)  |                        |
+
+## 工具
+
+| [Emacs](./tool/emacs.md) | [Github](./tool/github.md) | [nginx](./tool/nginx.md) | [Semver](./tool/semver.md) | [Vim](./tool/vim.md) |
+| :----------------------: | :------------------------: | :----------------------: | :------------------------: | :------------------: |
+| [XPath](./tool/xpath.md) |                            |                          |                            |                      |
+
+## 命令
+
+| [ADB, Android Debug Bridge](./command/adb.md) |     [Conan](./command/conan.md)     |    [Conda](./command/conda.md)    | [CocoaPods](./command/cocoapods.md) |         [Cargo](./command/cargo.md)         |
+| :-------------------------------------------: | :---------------------------------: | :-------------------------------: | :---------------------------------: | :-----------------------------------------: |
+|           [Curl](./command/curl.md)           |     [Chmod](./command/chmod.md)     |     [Cron](./command/cron.md)     |     [CMake](./command/cmake.md)     |          [Make](./command/make.md)          |
+|            [Git](./command/git.md)            |      [Grep](./command/grep.md)      |     [Find](./command/find.md)     |    [FFmpeg](./command/ffmpeg.md)    |          [Htop](./command/htop.md)          |
+|       [Homebrew](./command/homebrew.md)       |   [Netstat](./command/netstat.md)   | [iptables](./command/iptables.md) |        [jq](./command/jq.md)        |      [Justfile](./command/justfile.md)      |
+|           [Lsof](./command/lsof.md)           | [Mitmproxy](./command/mitmproxy.md) |   [Netcat](./command/netcat.md)   |       [Sed](./command/sed.md)       |       [OpenSSL](./command/openssl.md)       |
+|             [ps](./command/ps.md)             |   [Systemd](./command/systemd.md)   |      [SSH](./command/ssh.md)      |    [Screen](./command/screen.md)    |           [YUM](./command/yum.md)           |
+|            [CMD](./command/cmd.md)            |       [Zip](./command/zip.md)       |      [APT](./command/apt.md)      |       [tar](./command/tar.md)       | [Linux Command](./command/linux-command.md) |
+
+## 数据库
+
+| [MySQL](./database/mysql.md) | [MongoDB](./database/mongodb.md) | [Oracle](./database/oracle.md) | [PostgreSQL](./database/postgres.md) | [Redis](./database/redis.md) |
+| :--------------------------: | :------------------------------: | :----------------------------: | :----------------------------------: | :--------------------------: |
+|                              |                                  |                                |                                      |                              |
+
+## 快捷键
+
+| [Adobe Lightroom](./quicklink/adobe-lightroom.md) | [Adobe Photoshop](./quicklink/adobe-photoshop.md) |      [Adobe XD](./quicklink/adobe-xd.md)      | [Android Studio](./quicklink/android-studio.md) | [Firefox](./quicklink/firefox.md) |
+| :-----------------------------------------------: | :-----------------------------------------------: | :-------------------------------------------: | :---------------------------------------------: | :-------------------------------: |
+|           [Gmail](./quicklink/gmail.md)           |          [Gitlab](./quicklink/gitlab.md)          | [Google Chrome](./quicklink/google-chrome.md) | [IntelliJ IDEA](./quicklink/intelli-j-idea.md)  | [PyCharm](./quicklink/pycharm.md) |
+|          [Sketch](./quicklink/sketch.md)          |    [Sublime Text](./quicklink/sublime-text.md)    |        [VSCode](./quicklink/vscode.md)        |       [WebStorm](./quicklink/webstorm.md)       |   [Xcode](./quicklink/xcode.md)   |
+
+## 其它
+
+| [Aspect Ratio](./other/aspect-ratio.md) |   [Github Actions](./other/github-actions.md)   | [Gitlab CI/CD](./other/gitlab-ci.md) | [Colors Named](./other/colors-named.md) | [HTTP 状态码](./other/http-status-code.md) |
+| :-------------------------------------: | :---------------------------------------------: | :----------------------------------: | :-------------------------------------: | :----------------------------------------: |
+|  [HTML 字符实体](./other/html-char.md)  | [ISO 639-1 Language Code](./other/iso-639-1.md) |      [Emoji](./other/emoji.md)       |      [MIME types](./other/mime.md)      |   [Resolutions](./other/resolutions.md)    |
+
+## License
+
+MIT © [Wang Shuai](https://github.com/wscodinglover)
