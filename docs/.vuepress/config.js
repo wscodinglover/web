30cd68cfc697a65dfc00adff1f852b31fba9e03f
xichuang <1690680531@qq.com>
2023-06-13 20:37:43 +0800

    feat:test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,8 +14,7 @@ module.exports = {
   themeConfig: {
     sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
     lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
-    // displayAllHeaders: true,
-    // permalink: "/:year/:month/:day/:slug",
+
     nav: [
       {
         text: '前端',
